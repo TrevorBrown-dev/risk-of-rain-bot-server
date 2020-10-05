97b855a4b41b286e84e70c1fce051e32cef5945d
AndrewMcDonald-Dev <AndrewMcDonald-Dev@github.com>
2020-10-05 14:49:40 -0400

    Updated index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import Route from './Route';
-import { items, challenges, monsters, interactables, environments, drones, artifacts, survivors } from '../db';
+import { items, challenges, monsters, interactables, environments, drones, artifacts, survivors, skills } from '../db';
 export const itemsRoute = new Route('items', items).getRouter();
 export const interactablesRoute = new Route('interactables', interactables).getRouter();
 export const dronesRoute = new Route('drones', drones).getRouter();
@@ -8,6 +8,7 @@ export const challengesRoute = new Route('challenges', challenges).getRouter();
 export const monstersRoute = new Route('monsters', monsters).getRouter();
 export const environmentsRoute = new Route('environments', environments).getRouter();
 export const survivorsRoute = new Route('survivors', survivors).getRouter();
+export const skillsRoute = new Route('skills', skills).getRouter();
 
-const routes = [itemsRoute, dronesRoute, interactablesRoute, artifactsRoute, challengesRoute, monstersRoute, environmentsRoute, survivorsRoute];
+const routes = [itemsRoute, dronesRoute, interactablesRoute, artifactsRoute, challengesRoute, monstersRoute, environmentsRoute, survivorsRoute, skillsRoute];
 export default routes;
