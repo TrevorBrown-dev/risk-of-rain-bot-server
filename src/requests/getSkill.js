1a238dd0f71948db5096e0881f06a9163f5fefa9
Trevor Brown <trevorbrown-dev@github.com>
2020-10-06 14:51:29 -0400

    Working on tables to json

--- src/requests/getSkill.js
+++ src/requests/getSkill.js
@@ -3,42 +3,15 @@ import axios from 'axios';
 import e from 'express';
 
 const { JSDOM } = jsdom;
-
-const formatText = (text) => {
+/**
+ *
+ * @param {String} text A string containing all of the text related to a skill
+ */
+const makeSkill = (text) => {
+    if (!text) return text;
     text = text.trim().replace(/(\r\n|\n|\r){2,}/gm, '\n\n');
-    const health = 'Health';
-    const healthInfo = text.match(/(?<=Health\n)(.*){1}/);
-    const damage = 'Damage';
-    const damageInfo = text.match(/(?<=Damage\n)(.*){1}/);
-    const speed = 'Speed';
-    const speedInfo = text.match(/(?<=Speed\n)(.*){1}/);
-    const healthRegen = 'Health Regen';
-    const healthRegenInfo = text.match(/(?<=Health Regen\n)(.*){1}/);
-    const armor = 'Armor';
-    const armorInfo = text.match(/(?<=Armor\n)(.*){1}/);
-
-    return {
-        health: {
-            name: health,
-            value: healthInfo ? healthInfo[0] : '',
-        },
-        damage: {
-            name: damage,
-            value: damageInfo ? damageInfo[0] : '',
-        },
-        speed: {
-            name: speed,
-            value: speedInfo ? speedInfo[0] : '',
-        },
-        healthRegen: {
-            name: healthRegen,
-            value: healthRegenInfo ? healthRegenInfo[0] : '',
-        },
-        armor: {
-            name: armor,
-            value: armorInfo ? armorInfo[0] : '',
-        },
-    };
+    text = text.replace(/\n/, '');
+    return text;
 };
 
 const getSkill = async (params) => {
@@ -47,16 +20,19 @@ const getSkill = async (params) => {
         .catch((error) => {
             console.log(`Error accessing url: ${error.config.url}`);
             return {};
-            // console.log(error);
         });
     const page = new JSDOM(response.data);
     const { document } = page.window;
     let count = 1;
     let skills = [];
+
     let skillbox = document.querySelector(
         `.skillbox:nth-last-of-type(${count})`
     );
     while (skillbox) {
+        let prop = {
+            row: [],
+        };
         skillbox = document.querySelector(
             `.skillbox:nth-last-of-type(${count})`
         );
@@ -71,14 +47,43 @@ const getSkill = async (params) => {
                     `.skill:nth-last-of-type(${sCount})`
                 );
                 if (skill) {
-                    skills.unshift({ skillText: skill.textContent });
+                    prop.skillName = skill.querySelector('th').textContent;
+                    prop.skillImage = skill.querySelector('img').src;
+
+                    let rCount = 1;
+                    let row = skill.querySelector(
+                        `tr:nth-last-of-type(${sCount})`
+                    );
+
+                    while (row) {
+                        row = skill.querySelector(
+                            `tr:nth-last-of-type(${rCount})`
+                        );
+                        //TODO I'm so close to figuring out how to serialize these tables
+                        if (row) {
+                            console.log(row.outerHTML);
+                            prop.row.push({
+                                // title: row.querySelector('th').textContent,
+                                // text: row.querySelector('td').textContent,
+                            });
+                        }
+
+                        rcount++;
+                    }
+
+                    // console.log(skill.querySelector('img').src);
+
+                    skills.unshift(prop);
                 }
                 sCount++;
             }
-
             count++;
         }
     }
+
+    // for (let skill of skills) {
+    //     skill.text = makeSkill(skill.text);
+    // }
     // console.log(skills[2]);
 
     // const body = formatText(text);
